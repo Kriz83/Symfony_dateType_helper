49be4b1f966a3b518d606717a6759c6ae5cf6469
Krzysztof <krzysztofkorman83@gmail.com>
2018-07-25 15:14:46 +0200

    Add automatic search for Date fields
    Script now is searching for date fields from start,
    We do not need to invoke function dateFillAssistant by passing all field names from FORM.
    You can add script to base.html.twig and forgot about FORM dates :)

--- Symfony_fill_date_helper.js
+++ Symfony_fill_date_helper.js
@@ -12,50 +12,8 @@
 
     - It takes care to avoid pass more than x characters per field,
     - It clears field onclick
+    - It checks if only numbers are passed (red font when not)
 */
-
-//get start of id to pass to fill assistant (cut last string)
-let idStart = document.querySelector('.form-control').getAttribute('id');
-let res = idStart.split("_");
-res.pop();
-//reset string after cut
-for (let i=0; i < res.length; i++) {
-    if (i == 0) {
-        idStart = `${res[i]}`;
-    } else {
-        idStart = `${idStart}_${res[i]}`;
-    }        
-}
-
-function dateFillAssistant(idStart, fieldName, charsNumberDayMonth, charsNumberYear) {
-    //set values for inputs width
-    let multipler = 20;
-    let dayMonthFieldWidth = multipler * charsNumberDayMonth;
-    let yearFieldWidth = multipler * charsNumberYear;
-
-    //set day helper
-    let day = document.querySelector(`#${idStart}_${fieldName}_day`);
-    
-    preventOverfill(day, charsNumberDayMonth);
-    resetFieldData(day);
-    checkIsNumber(day);
-    styleDates('day', dayMonthFieldWidth);
-
-    //set month helper
-    let month = document.querySelector(`#${idStart}_${fieldName}_month`);        
-    preventOverfill(month, charsNumberDayMonth);
-    resetFieldData(month);
-    checkIsNumber(month);
-    styleDates('month', dayMonthFieldWidth);
-
-    //set year helper
-    let year = document.querySelector(`#${idStart}_${fieldName}_year`);  
-    preventOverfill(year, charsNumberYear);
-    resetFieldData(year);
-    checkIsNumber(year);  
-    styleDates('year', yearFieldWidth);      
-    
-}
 
 //check number of signs for field and prevent to pass more than is set
 function preventOverfill(fieldNameData, fieldLength) {
@@ -97,13 +55,68 @@ function styleDates(inputName, fieldWidth) {
         }
     }
 }
-//
 
-/*
- call date assistant 
- (pass "field name" from form "fieldName" (formType), number of chars for day and month, number of chars for year)
-*/
-dateFillAssistant(idStart, 'fieldName', 2, 4);
-dateFillAssistant(idStart, 'fieldName', 2, 4);
-//... pass as many as You want
+//set assistant to choosed fields
+function dateFillAssistant(fieldName, charsNumberDayMonth, charsNumberYear) {
+    //set values for inputs width
+    let multipler = 20;
+    let dayMonthFieldWidth = multipler * charsNumberDayMonth;
+    let yearFieldWidth = multipler * charsNumberYear;
+
+    //set day helper
+    let day = document.querySelector(`#${fieldName}_day`);
+    
+    preventOverfill(day, charsNumberDayMonth);
+    resetFieldData(day);
+    checkIsNumber(day);
+    styleDates('day', dayMonthFieldWidth);
+
+    //set month helper
+    let month = document.querySelector(`#${fieldName}_month`);        
+    preventOverfill(month, charsNumberDayMonth);
+    resetFieldData(month);
+    checkIsNumber(month);
+    styleDates('month', dayMonthFieldWidth);
+
+    //set year helper
+    let year = document.querySelector(`#${fieldName}_year`);  
+    preventOverfill(year, charsNumberYear);
+    resetFieldData(year);
+    checkIsNumber(year);  
+    styleDates('year', yearFieldWidth);      
+    
+}
+
+//cut name from id
+function getFormName(formId) {
+    let res = formId.split("_");
+    res.pop();//cut "day"
+    return res.join('_');
+}
+
+//get all dateType forms
+function findDateForms() {
+    let inputs = document.querySelectorAll(`input[name*='[day]']`); 
+    let idData;
+    let formNames = [];
+    if (inputs.length > 0) { 
+        for (let i=0; i < inputs.length; i++) { 
+            idData = inputs[i].getAttribute('id');                 
+            formNames[i] = getFormName(idData);  
+        }         
+    }
+    return formNames;
+}
+
+//get result of search
+let dateFormNames = findDateForms();
+//change attributes for all date fields (add dateFillAssistant)
+if (dateFormNames.length > 0) {
+    //only when names were found
+    let fieldName;
+    for (let i = 0; i < dateFormNames.length; i++) {
+        fieldName = dateFormNames[i];
+        dateFillAssistant(fieldName, 2, 4);
+    }
+}
 //enjoy
