306266aa50a1e4d91a345dd133cff789eee444d3
Krzysztof <krzysztofkorman83@gmail.com>
2018-08-03 10:08:58 +0200

    OPP Style

--- Symfony_fill_date_helper.js
+++ Symfony_fill_date_helper.js
@@ -15,108 +15,114 @@
     - It checks if only numbers are passed (red font when not)
 */
 
-//check number of signs for field and prevent to pass more than is set
-function preventOverfill(fieldNameData, fieldLength) {
-    fieldNameData.addEventListener("keyup", function(e){
-        
-        if (this.value.length >= fieldLength) { 
-            this.value = this.value.substr(0, fieldLength);
-        }
-    });
-}
-
-//reset field data onclick
-function resetFieldData(fieldNameData) {
-    fieldNameData.onclick = function(){        
-        fieldNameData.value = '';        
-    };
-}
-
-//check if only number is passed
-function checkIsNumber(fieldNameData) {
-    fieldNameData.addEventListener("keyup", function(e){        
-        if (isNaN(this.value)) { 
-            fieldNameData.style.color = "red";
-        } else {
-            fieldNameData.style.color = "black";
-        }
-    });
-}
-//
-
-//set css style for dates
-function styleDates(inputName, fieldWidth) {
-    let inputs = document.querySelectorAll(`input[name*='[${inputName}]']`); 
-    
-    if (inputs.length > 0) {                    
-        for (let i = 0; i < inputs.length; i++) {
-            inputs[i].style.position = 'relative';
-            inputs[i].style.width = `${fieldWidth}px`;
-        }
+class DateFields
+{
+    constructor(fieldName) {
+        this.fieldName = fieldName;
+        this.day = `#${fieldName}_day`;
+        this.month = `#${fieldName}_month`;
+        this.year = `#${fieldName}_year`;
+        this.fieldWidthMultipler = 20;
+        this.dayChars = 2; 
+        this.monthChars = 2; 
+        this.yearChars = 4; 
+        this.setUpAssistants();
     }
-}
 
-//set assistant to choosed fields
-function dateFillAssistant(fieldName, charsNumberDayMonth, charsNumberYear) {
-    //set values for inputs width
-    let multipler = 20;
-    let dayMonthFieldWidth = multipler * charsNumberDayMonth;
-    let yearFieldWidth = multipler * charsNumberYear;
-
-    //set day helper
-    let day = document.querySelector(`#${fieldName}_day`);
-    
-    preventOverfill(day, charsNumberDayMonth);
-    resetFieldData(day);
-    checkIsNumber(day);
-    styleDates('day', dayMonthFieldWidth);
-
-    //set month helper
-    let month = document.querySelector(`#${fieldName}_month`);        
-    preventOverfill(month, charsNumberDayMonth);
-    resetFieldData(month);
-    checkIsNumber(month);
-    styleDates('month', dayMonthFieldWidth);
-
-    //set year helper
-    let year = document.querySelector(`#${fieldName}_year`);  
-    preventOverfill(year, charsNumberYear);
-    resetFieldData(year);
-    checkIsNumber(year);  
-    styleDates('year', yearFieldWidth);      
-    
-}
-
-//cut name from id
-function getFormName(formId) {
-    let res = formId.split("_");
-    res.pop();//cut "day"
-    return res.join('_');
-}
-
-//get all dateType forms
-function findDateForms() {
-    let inputs = document.querySelectorAll(`input[name*='[day]']`); 
-    let idData;
-    let formNames = [];
-    if (inputs.length > 0) { 
-        for (let i=0; i < inputs.length; i++) { 
-            idData = inputs[i].getAttribute('id');                 
-            formNames[i] = getFormName(idData);  
-        }         
+    //invoke all assistants methods
+    setUpAssistants() {
+        this.dayAssistant();
+        this.monthAssistant();
+        this.yearAssistant();
     }
-    return formNames;
+
+    dayAssistant() {
+        let dayField = document.querySelector(this.day);
+        dayField.style.position = 'relative';
+        dayField.style.width = `${this.fieldWidthMultipler * this.dayChars}px`;
+        this.resetFieldData(dayField);
+        this.checkIsNumber(dayField);
+        this.preventOverfill(dayField, this.dayChars);
+    }
+
+    monthAssistant() {
+        let monthField = document.querySelector(this.month);
+        monthField.style.position = 'relative';
+        monthField.style.width = `${this.fieldWidthMultipler * this.monthChars}px`;
+        this.resetFieldData(monthField);
+        this.checkIsNumber(monthField);
+        this.preventOverfill(monthField, this.monthChars);
+    }
+
+    yearAssistant() {
+        let yearField = document.querySelector(this.year);
+        yearField.style.position = 'relative';
+        yearField.style.width = `${this.fieldWidthMultipler * this.yearChars}px`;
+        this.resetFieldData(yearField);
+        this.checkIsNumber(yearField);
+        this.preventOverfill(yearField, this.yearChars);
+    }
+
+    //reset field data onclick
+    resetFieldData(fieldNameData) {
+        fieldNameData.onclick = function(){        
+            fieldNameData.value = '';        
+        };
+    }
+
+    //check number of signs for field and prevent to pass more than is set
+    preventOverfill(fieldNameData, fieldLength) {
+        fieldNameData.addEventListener("keyup", function(e){        
+            if (this.value.length >= fieldLength) { 
+                this.value = this.value.substr(0, fieldLength);
+            }
+        });
+    }
+    
+    //check if only number is passed
+    checkIsNumber(fieldNameData) {
+        fieldNameData.addEventListener("keyup", function(e){        
+            if (isNaN(this.value)) { 
+                fieldNameData.style.color = "red";
+            } else {
+                fieldNameData.style.color = "black";
+            }
+        });
+    }
+
 }
 
-//get result of search
-let dateFormNames = findDateForms();
-//change attributes for all date fields (add dateFillAssistant)
-if (dateFormNames.length > 0) {
-    //only when names were found
-    let fieldName;
-    for (let i = 0; i < dateFormNames.length; i++) {
-        fieldName = dateFormNames[i];
-        dateFillAssistant(fieldName, 2, 4);
+class FindDateForms
+{
+    constructor() {
+        this.findDateForms();
     }
+    
+    //cut name from id
+    getFormName(formId) {
+        let res = formId.split("_");
+        res.pop();//cut "day"        
+        return res.join('_');
+    }
+
+    //get all dateType forms
+    findDateForms() {
+        let inputs = document.querySelectorAll(`input[name*='[day]']`); 
+        let idData;
+        let formObjects = [];
+
+        if (inputs.length > 0) { 
+            for (let i=0; i < inputs.length; i++) { 
+                //get form names and create objects for all
+                idData = this.getFormName(inputs[i].getAttribute('id')); 
+                formObjects[i] = new DateFields(idData);
+            }         
+        }
+        return formObjects;    
+    }
+
 }
+
+//set up assistants for all date fields
+const createObjectDates = new FindDateForms();
 //enjoy
